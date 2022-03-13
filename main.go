6088dbd87a4bbaa1e073c42d66fab7497bd07ffd
gamingbeast36 <64628398+unixchad@users.noreply.github.com>
2022-03-13 13:12:02 +0530

    bumped deps and articles folder cleanup

--- main.go
+++ main.go
@@ -1,77 +1,77 @@
 package main
 
 import (
-    "html/template"
-    "github.com/axyii/borked/utils"
-    "os"
-    "net/http"
-    "github.com/gin-contrib/gzip"
-    "github.com/gin-contrib/static"
-    "github.com/gin-gonic/gin"
+	"github.com/axyii/borked/utils"
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"net/http"
+	"os"
 )
 
 type Post struct {
-    Title   string
-    Content template.HTML
+	Title   string
+	Content template.HTML
 }
 
 func main() {
-    utils.Genpages()
-    allposts := utils.Getchrononames()
-    path,err := os.Getwd()
-    if err != nil{
-        utils.Logger.Println(err)
-    }
-    r := gin.Default()
-    r.Use(utils.Default())
-    r.Use(gzip.Gzip(gzip.DefaultCompression))
-    r.Use(gin.Logger())
-    r.Delims("{{", "}}")
-    r.SetFuncMap(template.FuncMap{
-        "formatname": utils.Formatasname,
-        "formatdate": utils.Formatasdate,
-    })
-    r.Use(static.Serve("/assets", static.LocalFile(path +"/assets", false)))
-    r.LoadHTMLGlob(path +"/templates/*.tmpl.html")
+	utils.Genpages()
+	allposts := utils.Getchrononames()
+	path, err := os.Getwd()
+	if err != nil {
+		utils.Logger.Println(err)
+	}
+	r := gin.Default()
+	r.Use(utils.Default())
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
+	r.Use(gin.Logger())
+	r.Delims("{{", "}}")
+	r.SetFuncMap(template.FuncMap{
+		"formatname": utils.Formatasname,
+		"formatdate": utils.Formatasdate,
+	})
+	r.Use(static.Serve("/assets", static.LocalFile(path+"/assets", false)))
+	r.LoadHTMLGlob(path + "/templates/*.tmpl.html")
 
-    r.GET("/", func(c *gin.Context) {
-        var posts []string
-        posts = allposts[:2]
+	r.GET("/", func(c *gin.Context) {
+		var posts []string
+		posts = allposts[:2]
 
-        c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
-            "posts": posts,
-        })
-    })
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"posts": posts,
+		})
+	})
 
-    r.GET("/articles/:postName", func(c *gin.Context) {
-        postName := c.Param("postName")
-        htmlfile, err := os.ReadFile(path +"/articles/" + postName)
-        if err != nil {
-            utils.Logger.Println(err)
-            c.HTML(http.StatusNotFound, "error.tmpl.html", nil)
-            return
-        }
-        postHTML := template.HTML(htmlfile)
-        postit := utils.Formatasname(postName) 
+	r.GET("/articles/:postName", func(c *gin.Context) {
+		postName := c.Param("postName")
+		htmlfile, err := os.ReadFile(path + "/articles/" + postName)
+		if err != nil {
+			utils.Logger.Println(err)
+			c.HTML(http.StatusNotFound, "error.tmpl.html", nil)
+			return
+		}
+		postHTML := template.HTML(htmlfile)
+		postit := utils.Formatasname(postName)
 
-        post := Post{Title: postit, Content: postHTML}
+		post := Post{Title: postit, Content: postHTML}
 
-        c.HTML(http.StatusOK, "post.tmpl.html", gin.H{
-            "Title":   post.Title,
-            "Content": post.Content,
-        })
-    })
-    r.GET("/articles", func(c *gin.Context){
+		c.HTML(http.StatusOK, "post.tmpl.html", gin.H{
+			"Title":   post.Title,
+			"Content": post.Content,
+		})
+	})
+	r.GET("/articles", func(c *gin.Context) {
 
-        c.HTML(http.StatusOK, "articles.tmpl.html", gin.H{
-            "posts": allposts,
-        })
-    })
-    r.NoRoute(func(c *gin.Context) {
-        c.HTML(http.StatusNotFound, "error.tmpl.html", nil)
-        return
+		c.HTML(http.StatusOK, "articles.tmpl.html", gin.H{
+			"posts": allposts,
+		})
+	})
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "error.tmpl.html", nil)
+		return
 
-    })
+	})
 
-    r.Run(":8080")
+	r.Run(":8080")
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,124 +1,132 @@
 package utils
 
-import(
-    "strings"
-    "path/filepath"
-    "io"
-    "fmt"
-    "bufio"
-    "sort"
-    "github.com/russross/blackfriday"
-    "os"
-    "time"
-    "regexp"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"github.com/russross/blackfriday"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+	"time"
 )
-type pageinfo struct{
-    pagename string
-    date time.Time
+
+type pageinfo struct {
+	pagename string
+	date     time.Time
 }
 
-func Formatasname(p string)string{
-    p = strings.TrimSuffix(p, filepath.Ext(p))
-    return fmt.Sprintf(p)
+func Formatasname(p string) string {
+	p = strings.TrimSuffix(p, filepath.Ext(p))
+	return fmt.Sprintf(p)
 }
 
-func Genpages(){
-    path,err := os.Getwd()
-    if err !=nil{
-        Logger.Println(err)
-    }
-    files, err := os.ReadDir(path+"/markdown/")
-    if err != nil{
-        Logger.Println(err)
-    }
-    for _,file := range files{
-        mdfile, err := os.ReadFile(path +"/markdown/" + file.Name())
-        if err != nil{
-            Logger.Println(err)
-        }
-        html := blackfriday.MarkdownCommon([]byte(mdfile))
-        err = os.WriteFile(path +"/articles/" + Formatasname(file.Name())+".html", html, 0644)
-        if err != nil {
-            Logger.Println(err)
-        }
-    }
+func Genpages() {
+	path, err := os.Getwd()
+	if err != nil {
+		Logger.Println(err)
+	}
+	files, err := os.ReadDir(path + "/markdown/")
+	if err != nil {
+		Logger.Println(err)
+	}
+	for _, file := range files {
+		mdfile, err := os.ReadFile(path + "/markdown/" + file.Name())
+		if err != nil {
+			Logger.Println(err)
+		}
+		html := blackfriday.MarkdownCommon([]byte(mdfile))
+		if _, err := os.Stat(path + "/articles/"); errors.Is(err, os.ErrNotExist) {
+			err := os.Mkdir(path+"/articles/", os.ModePerm)
+			if err != nil {
+				Logger.Println(err)
+			}
+		}
+		err = os.WriteFile(path+"/articles/"+Formatasname(file.Name())+".html", html, 0644)
+		if err != nil {
+			Logger.Println(err)
+		}
+	}
 }
 
-func Formatasdate(name string)(string){
-    wd := "/markdown/"
-    path,err := os.Getwd()
-    if err !=nil{
-        Logger.Println(err)
-    }
-    newname := Formatasname(name)
-    mainfile,err := os.Open(path+wd+newname+".md")
-    defer mainfile.Close()
-    if err != nil{
-        Logger.Println(err)
-    }
-    datex,err := ReadLine(mainfile,1)
-    if err != nil{
-        Logger.Println(err)
-    }
-    actualdates := findconvert(datex)
-    return actualdates.Format("02 January 2006")
+func Formatasdate(name string) string {
+	wd := "/markdown/"
+	path, err := os.Getwd()
+	if err != nil {
+		Logger.Println(err)
+	}
+	newname := Formatasname(name)
+	mainfile, err := os.Open(path + wd + newname + ".md")
+	defer mainfile.Close()
+	if err != nil {
+		Logger.Println(err)
+	}
+	datex, err := ReadLine(mainfile, 1)
+	if err != nil {
+		Logger.Println(err)
+	}
+	actualdates := findconvert(datex)
+	return actualdates.Format("02 January 2006")
 }
 
-func Getchrononames()([]string){
-    wd := "/markdown/"
-    path, err := os.Getwd()
-    if err != nil{
-        Logger.Println(err)
-    }
-    files, err := os.ReadDir(path + wd )
-    if err != nil{
-        Logger.Println(err)
-    }
-    nof := len(files)
-    var allpageinfo = make([]pageinfo,0,nof)
-    for _,file := range files{
-        mainfile,err := os.Open(path + wd +file.Name())
-        defer mainfile.Close()
-        if err != nil{
-            Logger.Println(err)
-        }
-        datex,err := ReadLine(mainfile,1)
-        if err != nil{
-            Logger.Println(err)
-        }
-        actualdates := findconvert(datex)
-        filename := fmt.Sprint(Formatasname(file.Name())+".html")
-        allpageinfo = append(allpageinfo,pageinfo{pagename:filename,date:actualdates})
-    }
-    sort.Slice(allpageinfo, func(i, j int) bool { return allpageinfo[i].date.After(allpageinfo[j].date) })
-    names := make([]string,0,nof)
-    for i := 0; i < nof; i++{
-        names = append(names,allpageinfo[i].pagename)
-    }
-    return names
+func Getchrononames() []string {
+	wd := "/markdown/"
+	path, err := os.Getwd()
+	if err != nil {
+		Logger.Println(err)
+	}
+	files, err := os.ReadDir(path + wd)
+	if err != nil {
+		Logger.Println(err)
+	}
+	nof := len(files)
+	var allpageinfo = make([]pageinfo, 0, nof)
+	for _, file := range files {
+		mainfile, err := os.Open(path + wd + file.Name())
+		defer mainfile.Close()
+		if err != nil {
+			Logger.Println(err)
+		}
+		datex, err := ReadLine(mainfile, 1)
+		if err != nil {
+			Logger.Println(err)
+		}
+		actualdates := findconvert(datex)
+		filename := fmt.Sprint(Formatasname(file.Name()) + ".html")
+		allpageinfo = append(allpageinfo, pageinfo{pagename: filename, date: actualdates})
+	}
+	sort.Slice(allpageinfo, func(i, j int) bool { return allpageinfo[i].date.After(allpageinfo[j].date) })
+	names := make([]string, 0, nof)
+	for i := 0; i < nof; i++ {
+		names = append(names, allpageinfo[i].pagename)
+	}
+	return names
 
 }
 
-func findconvert(datex string)(time.Time){
-    layoutISO := "2006-01-02"
-    re := regexp.MustCompile(`\d\d\d\d-\d\d-\d\d`) 
-    datestring := re.FindString(datex)
-    actualdates,err := time.Parse(layoutISO,datestring)
-    if err != nil{
-        Logger.Println(err)
-    }
-    return actualdates
+func findconvert(datex string) time.Time {
+	layoutISO := "2006-01-02"
+	re := regexp.MustCompile(`\d\d\d\d-\d\d-\d\d`)
+	datestring := re.FindString(datex)
+	actualdates, err := time.Parse(layoutISO, datestring)
+	if err != nil {
+		Logger.Println(err)
+	}
+	return actualdates
 
 }
 
 func ReadLine(r io.Reader, lineNum int) (line string, err error) {
-    sc := bufio.NewScanner(r)
-    lastLine := 0
-    for sc.Scan() {
-        lastLine++
-        if lastLine == lineNum {
-            return sc.Text(), sc.Err()
-        }
-    }
-    return line,io.EOF
+	sc := bufio.NewScanner(r)
+	lastLine := 0
+	for sc.Scan() {
+		lastLine++
+		if lastLine == lineNum {
+			return sc.Text(), sc.Err()
+		}
+	}
+	return line, io.EOF
 }
